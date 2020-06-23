7a47aaac0a230a9f46307e00ffef67c4a7c737cc
Annie Qiu <annieq2@illinois.edu>
2020-06-22 22:25:32 -0500

    Change font and title, add theme to UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from "react";
 import Header from "./components/Header";
+import Card from "./components/Header";
+import { ThemeProvider, CSSReset, theme } from "@chakra-ui/core";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+
+const breakpoints = ["360px", "768px", "1024px", "1440px"];
+breakpoints.sm = breakpoints[0];
+breakpoints.md = breakpoints[1];
+breakpoints.lg = breakpoints[2];
+breakpoints.xl = breakpoints[3];
+
+const newTheme = {
+  ...theme,
+  breakpoints,
+  fonts: {
+    body: "Open Sans, sans-serif",
+    heading: "Open Sans, serif",
+    mono: "Open Sans, monospace",
+  },
+};
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header headerItems={["Home", "Explore", "About"]} />
+        <ThemeProvider theme={newTheme}>
+          <CSSReset />
+          <Header headerItems={["Home", "Explore", "About"]} />
+        </ThemeProvider>
       </div>
     </Router>
   );
